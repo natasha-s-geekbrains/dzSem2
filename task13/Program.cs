f56ac055a2223cd9eaf2cd9a6318453f7088db7e
natasha_s <natasl1012@gmail.com>
2023-07-09 16:50:00 +0300

    added task 13

---
+++ task13/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, что третьей 
+// цифры нет.
+
+int GetRandomNumber()
+{
+    int random = new Random().Next();
+    return random;
+}
+
+int GetThirdDigit(int num1)
+{
+    while (num1 > 1000)
+    {
+        num1 = num1 / 10;
+    }
+    return num1 % 10;
+}
+
+void PrintResult(int num2)
+{
+    Console.WriteLine("Третья цифра числа - " + num2);
+}
+
+int rand = GetRandomNumber();
+Console.WriteLine("Случайное число = " + rand);
+
+if (rand < 100)
+{
+    Console.Write("Третьей цифры нет.");
+}
+else
+{
+    int thirdDigit = GetThirdDigit(rand);
+    PrintResult(thirdDigit);
+}
